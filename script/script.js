fbf89aad33af9a25ab1e893096e15c404c119b1a
Garo <Garo121090@gmail.com>
2018-07-02 21:08:44 +0400

    improved code

--- script/script.js
+++ script/script.js
@@ -2,28 +2,32 @@ let td = document.querySelectorAll("td"),
     indicator = true,
     arr = [],
     j = 0;
+function addValue() {
+    let x = this;
+    if (indicator) {
+        this.innerHTML = "X";
+        this.style.color = "#18c808";
+        j += 1;
+        addToArray(x);
+        if (j >= 5 && (arr[0] || arr[4] || arr[8])) {
+            lookCoincidence(indicator);
+        }
+        indicator = false;
+    } else {
+        this.innerHTML = "O";
+        this.style.color = "#c80214";
+        j += 1;
+        addToArray(x);
+        if (j >= 6) {
+            lookCoincidence(indicator);
+        }
+        indicator = true;
+    }
+    this.removeEventListener('click', addValue);
+}
 
 td.forEach((item) => {
-    item.addEventListener('click', function () {
-        let x = this;
-        if (indicator) {
-            this.innerHTML = "X";
-            this.style.color = "#18c808";
-            this.style.pointerEvents = "none";
-            j += 1;
-            addToArray(x);
-            lookCoincidence(indicator);
-            indicator = false;
-        } else {
-            this.innerHTML = "O";
-            this.style.color = "#c80214";
-            this.style.pointerEvents = "none";
-            j += 1;
-            addToArray(x);
-            lookCoincidence(indicator);
-            indicator = true;
-        }
-    })
+    item.addEventListener('click', addValue)
 });
 
 function addToArray(a) {
@@ -33,6 +37,9 @@ function addToArray(a) {
 
 function lookCoincidence(i) {
     if ((arr[0] === i && arr[1] === i && arr[2] === i) || (arr[0] === i && arr[3] === i && arr[6] === i) || (arr[0] === i && arr[4] === i && arr[8] === i) || (arr[1] === i && arr[4] === i && arr[7] === i) || (arr[2] === i && arr[4] === i && arr[6] === i) || (arr[2] === i && arr[5] === i && arr[8] === i) || (arr[3] === i && arr[4] === i && arr[5] === i) || (arr[6] === i && arr[7] === i && arr[8] === i)) {
+        td.forEach((item) => {
+            item.removeEventListener('click', addValue);
+        });
         setTimeout(function () {
             alert("you win");
             location.reload();
